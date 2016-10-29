82a1dbb7929017a1f3545dd4cd8404ae58e588a0
MThorning <matthewthorning@gmail.com>
2016-10-29 23:58:53 +0100

    mor bugs fixed

--- index.php
+++ index.php
@@ -56,7 +56,7 @@
                             <img src="img/jpg/eatsmart.jpg" alt="Eat Smart banner" class="smallScreenImg">
                         </div>
                     </div>
-                    <div class="row verticalAlign1">
+                    <div class="row verticalAlignLgOnly">
                         <div class="col-sm-12 col-md-6 textBox">
                             <h2 class="text-center">EAT SMART</h2>
                             <p class="text-justify"><em>‘You are what you eat’</em> (and drink)! <em>‘You can’t outrun a bad diet’</em>! These are a couple of common phrases banded about in nutrition / dietary circles and they are spot on! The fuel you put into your body has a massive impact on your health! Eat junk = poor health / Eat Smart = elevated health! That’s why here at Elevate Health &amp; Fitness one of our main priorities is to focus on what you eat and drink. We will provide you with a bespoke nutrition program that <strong style="text-decoration: underline;">WILL</strong> improve your health and vitality, providing your body with all the nutrients it needs to function in its optimal state. <a href="#"><em>Discover more...</em></a>
@@ -74,7 +74,7 @@
                             <img src="img/jpg/trainsmart.jpg" alt="Train Smart banner" class="smallScreenImg">
                         </div>
                     </div>
-                    <div class="row verticalAlign1">
+                    <div class="row verticalAlignLgOnly">
                         <div class="col-md-6">
                             <div class="bigScreenImg imgWrapper">
                                 <img class="curvedImg" src="img/jpg/trainer.jpg" alt="Personal Trainer">
@@ -92,7 +92,7 @@
                             <img src="img/jpg/livesmart.jpg" alt="Live Smart banner" class="smallScreenImg">
                         </div>
                     </div>
-                    <div class="row verticalAlign1">
+                    <div class="row verticalAlignLgOnly">
                         <div class="col-sm-12 col-md-6 textBox">
                             <h2 class="text-center">LIVE SMART</h2>
                             <p class="text-justify">So you eat and train smart, but the rest of your life is a hectic, stressful merry-go-round that leaves you exhausted and struggling to cope? To get the most out of your training and nutrition programs we emphasise the importance of lifestyle factors such as sleep and relaxation and the massive benefits they can have on your overall health and wellbeing. We will provide you with strategies to help you function at your best and get the most from your program! <a href="#"><em>Discover more...</em></a>
