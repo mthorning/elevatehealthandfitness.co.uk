4ffaa8dc73ce4b0887d69524c6d8b2e21872b74f
MThorning <matthewthorning@gmail.com>
2016-10-30 18:48:18 +0000

    slightly optimised service page images and played with drop caps for the numbers

--- index.php
+++ index.php
@@ -105,7 +105,7 @@
                         </div>
                     </div>
                     <br />
-                    <div class="jumbotron custom">
+                    <div class="jumbotron">
                         <h3>Start the journey to Elevate your health by contacting us to arrange your free, no obligation consultation.</h3>
                         <a class="btn btn-lg btnColor" href="contactUs.php">Contact us!</a>
                     </div>
--- services.php
+++ services.php
@@ -22,10 +22,10 @@
             <p>Your journey to elevate your health follows this process:</p>
             <div class="row verticalAlign">
                 <div class="col-xs-3">
-                    <img src="img/servicesPageImg/step1.png" alt="step 1 image" class="additionalPic">
+                    <img src="img/servicesPageImg/step1.jpg" alt="step 1 image" class="additionalPic">
                 </div>
                 <div class="col-xs-9">
-                    <p>1. Your journey begins with a free consultation where you explain to us what you are looking for and we explain what packages we have and what we can do for you!</p>
+                    <p><span class="dropcaps">1.</span>Your journey begins with a free consultation where you explain to us what you are looking for and we explain what packages we have and what we can do for you!</p>
                 </div>
             </div>
             <div class="row verticalAlign">
@@ -33,15 +33,15 @@
                     <img src="img/servicesPageImg/step2.jpg" alt="step 2 image" class="additionalPic">
                 </div>
                 <div class="col-xs-9">
-                    <p>2. Following this consultation, should you wish to elevate your health with us, you will be sent various questionnaires and a food diary to complete and we book your first session…</p>
+                    <p><span class="dropcaps">2.</span> Following this consultation, should you wish to elevate your health with us, you will be sent various questionnaires and a food diary to complete and we book your first session…</p>
                 </div>
             </div>
             <div class="row verticalAlign">
                 <div class="col-xs-3">
-                    <img src="img/servicesPageImg/step3.png" alt="step 3 image" class="additionalPic">
+                    <img src="img/servicesPageImg/step3.jpg" alt="step 3 image" class="additionalPic">
                 </div>
                 <div class="col-xs-9">
-                    <p>3. ….your first session consists of a thorough postural and physical assessment (nothing strenuous!!!).</p>
+                    <p><span class="dropcaps">3.</span> ...your first session consists of a thorough postural and physical assessment (nothing strenuous!!!).</p>
                 </div>
             </div>
             <div class="row verticalAlign">
@@ -49,15 +49,15 @@
                     <img src="img/servicesPageImg/step4.jpg" alt="step 4 image" class="additionalPic">
                 </div>
                 <div class="col-xs-9">
-                    <p>4. The information we ascertain from the questionnaires, food diary and postural and physical assessments is then used to design the Eat-Train-Live Smart program for you. This information is crucial in ensuring we can design the correct program to meet your needs and goals</p>
+                    <p><span class="dropcaps">4.</span> The information we ascertain from the questionnaires, food diary and postural and physical assessments is then used to design the Eat-Train-Live Smart program for you. This information is crucial in ensuring we can design the correct program to meet your needs and goals</p>
                 </div>
             </div>
             <div class="row verticalAlign">
                 <div class="col-xs-3">
-                    <img src="img/servicesPageImg/step5.png" alt="step 5 image" class="additionalPic">
+                    <img src="img/servicesPageImg/step5.jpg" alt="step 5 image" class="additionalPic">
                 </div>
                 <div class="col-xs-9">
-                    <p>5. Your bespoke program is then delivered and explained to you and the training can commence! The number of sessions you do with us, is up to you, but a minimum of at least one per week is recommended to be able provide feedback on the program and to ensure you are happy with the program. We can also check that you are carrying out the exercises correctly and it provides us with the opportunity to provide any further information and motivation to ensure you succeed with your program.</p>
+                    <p><span class="dropcaps">5.</span> Your bespoke program is then delivered and explained to you and the training can commence! The number of sessions you do with us, is up to you, but a minimum of at least one per week is recommended to be able provide feedback on the program and to ensure you are happy with the program. We can also check that you are carrying out the exercises correctly and it provides us with the opportunity to provide any further information and motivation to ensure you succeed with your program.</p>
                 </div>
             </div>
             <div class="row verticalAlign">
@@ -65,15 +65,15 @@
                     <img src="img/servicesPageImg/step6.jpg" alt="step 6 image" class="additionalPic">
                 </div>
                 <div class="col-xs-9">
-                    <p>6. The Eat-Train-Live Smart program will also be for you to follow at home, when you are not having a session delivered at Elevate Health &amp; Fitness.</p>
+                    <p><span class="dropcaps">6.</span> The Eat-Train-Live Smart program will also be for you to follow at home, when you are not having a session delivered at Elevate Health &amp; Fitness.</p>
                 </div>
             </div>
             <div class="row verticalAlign">
                 <div class="col-xs-3">
-                    <img src="img/servicesPageImg/step7.png" alt="step 7 image" class="additionalPic">
+                    <img src="img/servicesPageImg/step7.jpg" alt="step 7 image" class="additionalPic">
                 </div>
                 <div class="col-xs-9">
-                    <p>7. Your program is then fine tuned as you progress through the program to ensure it fits in with your lifestyle and other commitments, with regular re-assessment to ensure you are on the right track!</p>
+                    <p><span class="dropcaps">7.</span> Your program is then fine tuned as you progress through the program to ensure it fits in with your lifestyle and other commitments, with regular re-assessment to ensure you are on the right track!</p>
                 </div>
             </div>
         </div>
