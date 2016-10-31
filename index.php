e0b96b12d89f9f8bf4cd0aa474cdb405260f2a57
MThorning <matthewthorning@gmail.com>
2016-10-31 07:59:30 +0000

    made changes to font size and pic requested by Ken for the front page

--- index.php
+++ index.php
@@ -33,9 +33,9 @@
                     <div id="welcomeSection" class="textBox">
                         <h1>Welcome to Elevate Health &amp; Fitness</h1>
                         <br />
-                        <p class="text-left">Do you need help to:</p>
+                        <img src="img/jpg/happyPeople.jpg" alt="happy people" class="welcomeSectionImg">
+                        <h4 class="text-left display-1">Do you need help to:</h4>
                         <div id="bullets">
-                            <img src="img/jpg/happyPeople.jpg" alt="happy people" class="welcomeSectionImg">
                             <p>• reduce your stress levels?</p>
                             <p> • alleviate back pain?</p>
                             <p> • overcome constant fatigue and low energy levels?</p>
@@ -44,7 +44,8 @@
                             <p> • improve your fitness/sports performance levels?</p>
                         </div>
                         <br />
-                        <p class="text-center"><strong>If so, you’ve come to the right place!</strong></p>
+                        <h3 class="text-center">If so, you’ve come to the right place!</h3>
+                        <br />
                         <br />
                         <p class="text-justify">Here at Elevate Health &amp; Fitness, we provide bespoke, holistic personal training programs incorporating exercise, nutrition and lifestyle strategies, designed to help you achieve your health and fitness goals.</p>
                         <p class="text-justify">With 1-to-1 personal training based in Truro or at a venue suitable for you, we follow an eat-train-live smart philosophy to help you reach your peak health and fitness, improving your sense of well-being and leading to an improved quality of life for you!</p>
@@ -59,8 +60,8 @@
                     <div class="row verticalAlignLgOnly">
                         <div class="col-sm-12 col-md-6 textBox">
                             <h2 class="text-center">EAT SMART</h2>
-                            <p class="text-justify"><em>‘You are what you eat’</em> (and drink)! <em>‘You can’t outrun a bad diet’</em>! These are a couple of common phrases banded about in nutrition / dietary circles and they are spot on! The fuel you put into your body has a massive impact on your health! Eat junk = poor health / Eat Smart = elevated health! That’s why here at Elevate Health &amp; Fitness one of our main priorities is to focus on what you eat and drink. We will provide you with a bespoke nutrition program that <strong style="text-decoration: underline;">WILL</strong> improve your health and vitality, providing your body with all the nutrients it needs to function in its optimal state. <a href="#"><em>Discover more...</em></a>
-                            </p>
+                            <h4 class="text-left"><em>‘You are what you eat’</em> (and drink)! <em>‘You can’t outrun a bad diet’</em>! These are a couple of common phrases banded about in nutrition / dietary circles and they are spot on! The fuel you put into your body has a massive impact on your health! Eat junk = poor health / Eat Smart = elevated health! That’s why here at Elevate Health &amp; Fitness one of our main priorities is to focus on what you eat and drink. We will provide you with a bespoke nutrition program that <strong style="text-decoration: underline;">WILL</strong> improve your health and vitality, providing your body with all the nutrients it needs to function in its optimal state. <a href="#"><em>Discover more...</em></a>
+                            </h4>
                         </div>
                         <div class="col-md-6 ">
                             <div class="bigScreenImg imgWrapper">
@@ -82,8 +83,8 @@
                         </div>
                         <div class="col-sm-12 col-md-6 textBox">
                             <h2 class="text-center">TRAIN SMART</h2>
-                            <p class="text-justify">Whether you are returning from injury, looking to alleviate postural problems or back pain, want to improve your fitness or sports performance, or even just trying to improve your energy levels, here at Elevate Health &amp; Fitness, we will provide you with a unique exercise program based on your needs, goals and assessments taken at the beginning of your journey. This program will ensure you can function at your best and will be tailored around your life. <a href="#"><em>Discover more...</em></a>
-                            </p>
+                            <h4 class="text-left">Whether you are returning from injury, looking to alleviate postural problems or back pain, want to improve your fitness or sports performance, or even just trying to improve your energy levels, here at Elevate Health &amp; Fitness, we will provide you with a unique exercise program based on your needs, goals and assessments taken at the beginning of your journey. This program will ensure you can function at your best and will be tailored around your life. <a href="#"><em>Discover more...</em></a>
+                            </h4>
                         </div>
                     </div>
                     <hr />
@@ -95,8 +96,8 @@
                     <div class="row verticalAlignLgOnly">
                         <div class="col-sm-12 col-md-6 textBox">
                             <h2 class="text-center">LIVE SMART</h2>
-                            <p class="text-justify">So you eat and train smart, but the rest of your life is a hectic, stressful merry-go-round that leaves you exhausted and struggling to cope? To get the most out of your training and nutrition programs we emphasise the importance of lifestyle factors such as sleep and relaxation and the massive benefits they can have on your overall health and wellbeing. We will provide you with strategies to help you function at your best and get the most from your program! <a href="#"><em>Discover more...</em></a>
-                            </p>
+                            <h4 class="text-left">So you eat and train smart, but the rest of your life is a hectic, stressful merry-go-round that leaves you exhausted and struggling to cope? To get the most out of your training and nutrition programs we emphasise the importance of lifestyle factors such as sleep and relaxation and the massive benefits they can have on your overall health and wellbeing. We will provide you with strategies to help you function at your best and get the most from your program! <a href="#"><em>Discover more...</em></a>
+                            </h4>
                         </div>
                         <div class="col-md-6">
                             <div class="bigScreenImg imgWrapper">
