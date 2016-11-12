df775e68187365217ce049f717071bcc7dc13353
MThorning <matthewthorning@gmail.com>
2016-11-12 09:39:36 +0000

    fixed menu dropdown overflow issue

--- components/header.php
+++ components/header.php
@@ -1,12 +1,11 @@
 <?php include "modules/inc.title.php"; ?>
 <nav role="navigation" class="navbar navbar-default navbar-fixed-top">
     <div id="navWrapper">
-<!--        temporary indicator-->
-        <div id="screenIndicator">
-                
-        </div>
+
+    <!--        temporary indicator-->
+        <div id="screenIndicator"></div>
         
-        <div id="etlSmartPage" class="navbar-header">
+        <div class="navbar-header">
             <a class="navbar-brand" href="#"><a href="index.php"><img src="img/png/navbarLogo.png" id="navbarLogo"></a></a>
             <button id="navToggleButton" type="button" class="navbar-toggle" data-target="#navbarCollapse" data-toggle="collapse">
                 <span class="sr-only">Toggle navigation</span>
@@ -32,8 +31,10 @@
                     </ul>
                 </li>
                 <li id="services"><a href="services.php">Services</a></li>
+<!--
                 <li id="testimonials"><a href="testimonials.php">Testimonials</a></li>
                 <li id="FAQ"><a href="#">FAQ</a></li>
+-->
                 <li id="contactUs"><a href="contactUs.php">Contact Us</a></li>
             </ul>
         </div>
